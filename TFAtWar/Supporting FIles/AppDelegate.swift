7cf104582f760e2e7b1966af62d53d8066c92565
Guillermo Sáenz <gsaenz@proatomicdev.com>
2020-12-15 22:39:45 -0500

    Changed order of files to reflect order of appearance

--- TFAtWar/Supporting FIles/AppDelegate.swift
+++ TFAtWar/Supporting FIles/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
+    // main dependencies container
     private let appDependenciesContainer = AppDependenciesContainer()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
